961009d483d308703b81d7ae6ca291678c716d78
Jesse Horner <jrhorner@pm.me>
2021-12-01 15:20:51 -0500

    Intcode module

---
+++ utils/intcode/intcode.go
@@ -0,0 +1,98 @@
+package intcode
+
+type Computer struct {
+	memory       *[]int   // this is the programs memory space
+	pointer      int      // pointer to current memory address
+	input        chan int // input channel
+	output       chan int // output channel
+	instruction  int      // the raw opcode instruction
+	relativeBase int      // relative base for parameter 2 operations
+}
+
+const (
+	memoryMultiplier = 50
+)
+
+// initialize a new computer instance
+func NewComputer(program *[]int) Computer {
+	// create the extended memory space and copy the program (intcode) into it
+	memory := make([]int, len(*program)*memoryMultiplier)
+	copy(memory, *program)
+
+	return Computer{
+		memory:       &memory,
+		pointer:      0,
+		input:        make(chan int, 1),
+		output:       make(chan int),
+		instruction:  0,
+		relativeBase: 0}
+}
+
+func (c *Computer) Run() {
+loop: // loop until the program ends
+	for {
+		c.instruction = c.Read(1)     // read the first instruction in parameter 1 mode
+		opcode := c.instruction % 100 // get the opcode without the parameters
+		switch opcode {
+		case 1:
+			c.Add()
+		case 2:
+			c.Multiply()
+		// case 3: c.PutInput()
+		// case 4: c.PutOutput()
+		// case 5: c.JumpIfTrue()
+		// case 6: c.JumpIfFalse()
+		// case 7: c.LessThan()
+		// case 8: c.Equals()
+		// case 9: c.RelativeBaseOffset()
+		case 99:
+			break loop // self explanatory, break the loop created above
+		}
+	}
+	close(c.output) // close the output channel once the program ends
+	return
+}
+
+// return the correct address based on the parameter mode
+func (c *Computer) Parameter(mode int) int {
+	var address int
+	switch mode {
+	case 0: // position mode
+		address = (*c.memory)[c.pointer]
+	case 1: // immediate mode
+		address = c.pointer
+	case 2: // relative mode
+		address = (*c.memory)[c.pointer] + c.relativeBase
+	}
+	c.pointer++ // move the pointer to the next address
+	return address
+}
+
+// returns the value at the correct memory address
+func (c *Computer) Read(mode int) int {
+	address := c.Parameter(mode)
+	return (*c.memory)[address]
+}
+
+// writes the value to the correct memory address
+func (c *Computer) Write(value int, mode int) {
+	address := c.Parameter(mode)
+	(*c.memory)[address] = value
+	return
+}
+
+// add 2 values, write them to memory
+func (c *Computer) Add() {
+	a := c.Read((c.instruction / 100) % 10)
+	b := c.Read((c.instruction / 1000) % 10)
+
+	c.Write(a+b, (c.instruction/10000)%10)
+}
+
+// multiply 2 values, write them to memory
+func (c *Computer) Multiply() {
+	a := c.Read((c.instruction / 100) % 10)
+	b := c.Read((c.instruction / 1000) % 10)
+
+	c.Write(a*b, (c.instruction/10000)%10)
+}
