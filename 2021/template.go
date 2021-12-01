5ec866eda9b2821fc20e61e46cff78dd93905a24
Jesse Horner <jrhorner@pm.me>
2021-12-01 10:02:34 -0500

    Added input parsing and print statements for part 1 and 2.

--- 2021/template.go
+++ 2021/template.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"strings"
 )
 
 func main() {
+	input, _ := ioutil.ReadFile("input")
+	in := strings.Split(strings.TrimSpace(string(input)), "\n")
+	fmt.Println("Part 1:", in)
+	// fmt.Println("Part 2:", input)
 	fmt.Println("Happy Holidays 2021!")
 }
