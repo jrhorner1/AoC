5d5c4a47d698551260bb3cd90739b3d49db0b8a6
Jesse Horner <jrhorner@pm.me>
2021-12-03 12:45:21 -0500

    Removed parsing to int slice then back to string.

--- 2021/solutions/3.go
+++ 2021/solutions/3.go
@@ -3,75 +3,52 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math"
 	"strconv"
 	"strings"
 )
 
 func main() {
-	diag := parse("2021/input/3")
-	bits := len(diag[0])
+	input, _ := ioutil.ReadFile("2021/input/3")
+	diag := strings.Split(strings.TrimSpace(string(input)), "\n")
 
-	gamma, epsilon := "", ""
-	for i := 0; i < bits; i++ {
-		bit0 := 0
-		for _, row := range diag {
-			if row[i] == 0 {
-				bit0++
-			}
-		}
-		if bit0 >= len(diag)/2 {
-			gamma += "0"
-			epsilon += "1"
-		} else {
-			gamma += "1"
-			epsilon += "0"
+	var bitSums []int
+	for i := 0; i < len(diag[0]); i++ {
+		bitSums = append(bitSums, 0)
+		for _, bit := range diag {
+			bitSums[i] += int(bit[i]) - int('0')
 		}
 	}
-	gammaRate, _ := strconv.ParseUint(gamma, 2, 32)
-	epsilonRate, _ := strconv.ParseUint(epsilon, 2, 32)
-	powerConsumption := gammaRate * epsilonRate
-	fmt.Println("Part 1:", powerConsumption)
+	var gammaRate, epsilonRate int
+	for i, sum := range bitSums {
+		if sum > len(diag)/2 {
+			gammaRate += int(math.Pow(2, float64(len(bitSums)-1-i)))
+		} else {
+			epsilonRate += int(math.Pow(2, float64(len(bitSums)-1-i)))
+		}
+	}
+	fmt.Println("Part 1:", gammaRate*epsilonRate)
 
 	oxyGenRating := getRating(diag, true, 0)
 	co2ScrubRating := getRating(diag, false, 0)
-	lifeSupportRating := oxyGenRating * co2ScrubRating
-	fmt.Println("Part 2:", lifeSupportRating)
+	fmt.Println("Part 2:", oxyGenRating*co2ScrubRating)
+
 	fmt.Println("Happy Holidays 2021!")
 }
 
-func parse(file string) [][]int {
-	input, _ := ioutil.ReadFile(file)
-	in := strings.Split(strings.TrimSpace(string(input)), "\n")
-	diag := [][]int{}
-	for _, s := range in {
-		row := []int{}
-		for _, r := range s {
-			switch r {
-			case '0':
-				row = append(row, 0)
-			case '1':
-				row = append(row, 1)
-			}
-		}
-		diag = append(diag, row)
-	}
-	return diag
-}
-
-func getRating(diagData [][]int, majority bool, index int) int {
+func getRating(data []string, majority bool, index int) int {
 	// base case
-	if len(diagData) == 1 {
-		binaryString := parseBin(diagData[0])
-		rating, _ := strconv.ParseInt(binaryString, 2, 32)
+	if len(data) == 1 {
+		rating, _ := strconv.ParseInt(data[0], 2, 32)
 		return int(rating)
 	}
 	// divide the data into 2 slices based on bit value at index
-	var zerosData, onesData [][]int
-	for _, binarySlice := range diagData {
-		if binarySlice[index] == 0 {
-			zerosData = append(zerosData, binarySlice)
+	var zerosData, onesData []string
+	for _, binaryString := range data {
+		if binaryString[index] == '0' {
+			zerosData = append(zerosData, binaryString)
 		} else {
-			onesData = append(onesData, binarySlice)
+			onesData = append(onesData, binaryString)
 		}
 	}
 	// recurse with the data set that matches the bit criteria
@@ -79,12 +56,4 @@ func getRating(diagData [][]int, majority bool, index int) int {
 		return getRating(zerosData, majority, index+1)
 	}
 	return getRating(onesData, majority, index+1)
-}
-
-func parseBin(binarySlice []int) string {
-	binaryString := ""
-	for _, bit := range binarySlice {
-		binaryString += strconv.Itoa(bit)
-	}
-	return binaryString
 }
