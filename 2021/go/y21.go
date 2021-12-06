f1d9c0bd99d2be04550786c413baa3db4b2054e9
Jesse Horner <jrhorner@pm.me>
2021-12-05 19:23:19 -0500

    Package naming shenanigans

--- 2021/go/y21.go
+++ 2021/go/y21.go
@@ -3,10 +3,10 @@ package y21
 import (
 	"fmt"
 
-	"github.com/jrhorner1/AoC/2021/go/day01"
+	"github.com/jrhorner1/AoC/2021/go/day1"
 )
 
 func Day1() {
-	fmt.Println("Part 1:", day01.Part1())
-	fmt.Println("Part 2:", day01.Part2())
+	fmt.Println("Part 1:", day1.Part1())
+	fmt.Println("Part 2:", day1.Part2())
 }
