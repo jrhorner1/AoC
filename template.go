ebb4746ef784757e685a0ed854456721ad90fc8c
Jesse Horner <jrhorner@pm.me>
2021-12-07 10:15:41 -0500

    No longer exporting puzzle and import functions.

--- template.go
+++ template.go
@@ -8,20 +8,20 @@ import (
 var filename = "0000/input/0"
 
 func Part1() int {
-	return Puzzle(Input(filename), false)
+	return puzzle(input(filename), false)
 }
 
 func Part2() int {
-	return Puzzle(Input(filename), true)
+	return puzzle(input(filename), true)
 }
 
-func Input(file string) []string {
+func input(file string) []string {
 	input, _ := ioutil.ReadFile(file)
 	output := strings.Split(strings.TrimSpace(string(input)), "\n")
 	return output
 }
 
-func Puzzle(input []string, part2 bool) int {
+func puzzle(input []string, part2 bool) int {
 	// put code here
 	if part2 {
 		return 42
