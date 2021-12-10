69df5832073505e0e272295412f03bc1da742230
Jesse Horner <jrhorner@pm.me>
2021-12-10 12:40:12 -0500

    Day 3 finished.

--- 2017/go/day3/3.go
+++ 2017/go/day3/3.go
@@ -1,7 +1,6 @@
 package day3
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 
@@ -9,36 +8,37 @@ import (
 	geom "github.com/jrhorner1/AoC/pkg/math/geometry"
 )
 
-type memory struct {
-	address geom.Point
-	id      int
-	value   int
-}
-
 func Puzzle(input *[]byte, part2 bool) int {
-	in, _ := strconv.Atoi(strings.TrimSpace(string(*input)))
+	target, _ := strconv.Atoi(strings.TrimSpace(string(*input)))
 	if part2 {
-		// spiral := []memory{{geom.Point{0, 0}, 1, 1}}
-		// neighbors := []geom.Point{{1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}}
-		total := 0
-		// for i := 0; i < 1000; i++ {
-		// 	if total >= in {
-		// 		break
-		// 	}
-		// 	if spiral[i].addr {
-		// 		//
-		// 	}
-		// }
-		if total == 312453 {
-			return total
-		} else {
-			fmt.Println("You fucked up.")
-			return 312453
+		current := geom.Point{0, 0}
+		spiral := make(map[geom.Point]int)
+		spiral[current] = 1
+		neighbors := []geom.Point{{1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}}
+		for {
+			if spiral[current] >= target {
+				return spiral[current]
+			}
+			if current.X-current.Y >= 0 && current.X+current.Y <= 0 { // right
+				current.X++
+			} else if current.X-current.Y > 0 && current.X+current.Y >= 0 { // up
+				current.Y++
+			} else if current.X-current.Y <= 0 && current.X+current.Y > 0 { // left
+				current.X--
+			} else if current.X-current.Y <= 0 && current.X+current.Y <= 0 { // down
+				current.Y--
+			}
+			for _, n := range neighbors {
+				neighbor := geom.Point{current.X + n.X, current.Y + n.Y}
+				if v, found := spiral[neighbor]; found {
+					spiral[current] += v
+				}
+			}
 		}
 	}
 	// determine the ring that the destination is on using the odd square pattern in the botton right direction on the grid
 	oddsqr := 1
-	for oddsqr*oddsqr < in {
+	for oddsqr*oddsqr < target {
 		oddsqr += 2
 	}
 	// calculate the corners of the ring
@@ -48,7 +48,7 @@ func Puzzle(input *[]byte, part2 bool) int {
 	}
 	//
 	for _, p := range pivots {
-		dist := math.IntAbs(p - in)
+		dist := math.IntAbs(p - target)
 		if dist <= (oddsqr-1)/2 {
 			return oddsqr - 1 - dist
 		}
