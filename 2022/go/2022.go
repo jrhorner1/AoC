d0096b5bef7c7ba94ca6e4198907b0491e2c20fe
Jesse Horner <jrhorner@pm.me>
2022-12-02 10:30:25 -0500

    Day 2 complete ⭐️

--- 2022/go/2022.go
+++ 2022/go/2022.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"github.com/jrhorner1/AoC/2022/go/day1"
+	"github.com/jrhorner1/AoC/2022/go/day2"
 )
 
 func Run(year, day *int) {
@@ -11,6 +12,9 @@ func Run(year, day *int) {
 	case 1:
 		input, _ := ioutil.ReadFile("2022/input/1")
 		fmt.Printf("\t%d Day %d solutions\nPart 1: %d\nPart 2: %d\n", *year, *day, day1.Puzzle(&input, false), day1.Puzzle(&input, true))
+	case 2:
+		input, _ := ioutil.ReadFile("2022/input/2")
+		fmt.Printf("\t%d Day %d solutions\nPart 1: %d\nPart 2: %d\n", *year, *day, day2.Puzzle(&input, false), day2.Puzzle(&input, true))
 	default:
 		panic("unimplemented")
 	case 26, 27, 28, 29, 30, 31:
