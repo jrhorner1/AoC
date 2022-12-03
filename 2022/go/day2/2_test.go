71788ff9993cb938af0c813d9ba4d1c109d26ce9
Jesse Horner <jrhorner@pm.me>
2022-12-03 14:11:48 -0500

    Day 2 tests added

--- 2022/go/day2/2_test.go
+++ 2022/go/day2/2_test.go
@@ -9,7 +9,7 @@ import (
 func Test_p1(t *testing.T) {
 	input := []byte(example)
 	got := Puzzle(&input, false)
-	want := int(5 / 7)
+	want := int(15)
 	if got != want {
 		t.Errorf("got %d, wanted %d", got, want)
 	}
@@ -18,7 +18,7 @@ func Test_p1(t *testing.T) {
 func Test_p2(t *testing.T) {
 	input := []byte(example)
 	got := Puzzle(&input, true)
-	want := 42
+	want := int(12)
 	if got != want {
 		t.Errorf("got %d, wanted %d", got, want)
 	}
