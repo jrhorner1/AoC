bca8e8eedb6f66fa47a83754d9ab2436039c092a
Jesse Horner <jrhorner@pm.me>
2022-12-06 21:14:58 -0500

    Day 1 complete

--- 2022/rust/day1/src/main.rs
+++ 2022/rust/day1/src/main.rs
@@ -6,23 +6,33 @@ use std::fs;
 
 fn main() {
     let filepath = "../../input/1";
-    let raw_input: String = fs::read_to_string(filepath)
+    let raw_input = fs::read_to_string(filepath)
         .expect("Should have been able to read the file");
     let input = raw_input.split("\n\n");
-    for inp in input {
-        println!("{}", inp                                          );
-    }
-
-    let mut calories = vec![];
-    for elf in input {
-        let snacks = elf.split("\n");
-        let mut cals = 0;
+    let mut elves: Vec<u32> = Vec::new();
+    for bag in input {
+        let snacks = bag.split("\n");
+        let mut total_calories = 0;
         for snack in snacks {
-            let snack: u32 = snack.trim().parse()
-                .expect("Something went wrong with the conversion.");
-            cals = cals + snack;
-        };
-        calories.push(cals)
+            let calories: u32 = match snack.parse() {
+                Ok(calories) => calories,
+                Err(_) => continue,
+            };
+            total_calories += calories
+        }
+        elves.push(total_calories)
     }
-    println!("{}", calories[1]);
+    elves.sort();
+    let most_calories = elves.last().unwrap();
+    println!("Part 1: {}", most_calories);
+    let mut top_three_total = 0;
+    let mut count = 0;
+    for elf in elves.iter().rev() {
+        count += 1;
+        top_three_total += elf;
+        if count == 3 {
+            break
+        }
+    }
+    println!("Part 2: {}", top_three_total)
 }
