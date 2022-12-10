abcd8b1228a6c12091c76b812ae25cca0fafab55
Jesse Horner <jrhorner@pm.me>
2022-12-10 10:26:34 -0500

    Template changes and output using logrus

--- 2022/go/2022.go
+++ 2022/go/2022.go
@@ -21,36 +21,36 @@ func Run(year, day *int) {
 	switch *day {
 	case 1:
 		input, _ := ioutil.ReadFile("2022/input/1")
-		log.Infof("%d - Day %d - Part 1: %v", *year, *day, day1.Puzzle(&input, false))
-		log.Infof("%d - Day %d - Part 1: %v", *year, *day, day1.Puzzle(&input, true))
+		output(year, day, 1, fmt.Sprint(day1.Puzzle(&input, false)))
+		output(year, day, 2, fmt.Sprint(day1.Puzzle(&input, true)))
 	case 2:
 		input, _ := ioutil.ReadFile("2022/input/2")
-		log.Infof("%d - Day %d - Part 1: %v", *year, *day, day2.Puzzle(&input, false))
-		log.Infof("%d - Day %d - Part 1: %v", *year, *day, day2.Puzzle(&input, true))
+		output(year, day, 1, fmt.Sprint(day2.Puzzle(&input, false)))
+		output(year, day, 2, fmt.Sprint(day2.Puzzle(&input, true)))
 	case 3:
 		input, _ := ioutil.ReadFile("2022/input/3")
-		log.Infof("%d - Day %d - Part 1: %v", *year, *day, day3.Puzzle(&input, false))
-		log.Infof("%d - Day %d - Part 1: %v", *year, *day, day3.Puzzle(&input, true))
+		output(year, day, 1, fmt.Sprint(day3.Puzzle(&input, false)))
+		output(year, day, 2, fmt.Sprint(day4.Puzzle(&input, true)))
 	case 4:
 		input, _ := ioutil.ReadFile("2022/input/4")
-		log.Infof("%d - Day %d - Part 1: %v", *year, *day, day4.Puzzle(&input, false))
-		log.Infof("%d - Day %d - Part 1: %v", *year, *day, day4.Puzzle(&input, true))
+		output(year, day, 1, fmt.Sprint(day4.Puzzle(&input, false)))
+		output(year, day, 2, fmt.Sprint(day4.Puzzle(&input, true)))
 	case 5:
 		input, _ := ioutil.ReadFile("2022/input/5")
-		log.Infof("%d - Day %d - Part 1: %v", *year, *day, day5.Puzzle(&input, false))
-		log.Infof("%d - Day %d - Part 1: %v", *year, *day, day5.Puzzle(&input, true))
+		output(year, day, 1, fmt.Sprint(day5.Puzzle(&input, false)))
+		output(year, day, 2, fmt.Sprint(day5.Puzzle(&input, true)))
 	case 6:
 		input, _ := ioutil.ReadFile("2022/input/6")
-		log.Infof("%d - Day %d - Part 1: %v", *year, *day, day6.Puzzle(&input, 4))
-		log.Infof("%d - Day %d - Part 1: %v", *year, *day, day6.Puzzle(&input, 14))
+		output(year, day, 1, fmt.Sprint(day6.Puzzle(&input, 4)))
+		output(year, day, 2, fmt.Sprint(day6.Puzzle(&input, 14)))
 	case 7:
 		input, _ := ioutil.ReadFile("2022/input/7")
-		log.Infof("%d - Day %d - Part 1: %v", *year, *day, day7.Puzzle(&input, false))
-		log.Infof("%d - Day %d - Part 1: %v", *year, *day, day7.Puzzle(&input, true))
+		output(year, day, 1, fmt.Sprint(day7.Puzzle(&input, false)))
+		output(year, day, 2, fmt.Sprint(day7.Puzzle(&input, true)))
 	case 8:
 		input, _ := ioutil.ReadFile("2022/input/8")
-		log.Infof("%d - Day %d - Part 1: %v", *year, *day, day8.Puzzle(&input, false))
-		log.Infof("%d - Day %d - Part 1: %v", *year, *day, day8.Puzzle(&input, true))
+		output(year, day, 1, fmt.Sprint(day8.Puzzle(&input, false)))
+		output(year, day, 2, fmt.Sprint(day8.Puzzle(&input, true)))
 	case 9:
 		input, _ := ioutil.ReadFile("2022/input/9")
 		output(year, day, 1, fmt.Sprint(day9.Puzzle(&input, 2)))
@@ -58,7 +58,8 @@ func Run(year, day *int) {
 	case 10:
 		input, _ := ioutil.ReadFile("2022/input/10")
 		p1, p2 := day10.Puzzle(&input)
-		fmt.Printf("\t%d Day %d solutions\nPart 1: %d\nPart 2: \n\n%v\n", *year, *day, p1, p2)
+		output(year, day, 1, fmt.Sprint(p1))
+		output(year, day, 2, fmt.Sprint("\n", p2))
 	default:
 		panic("unimplemented")
 	case 26, 27, 28, 29, 30, 31:
@@ -67,5 +68,5 @@ func Run(year, day *int) {
 }
 
 func output(year, day *int, part int, solution string) {
-	log.Infof("%d - Day %d - Part %d: %s", *year, *day, part, solution)
+	log.Infof("%d Day %d|Part %d: %s", *year, *day, part, solution)
 }
