da490afa01ef6d088a22d95ff326be7d2f8682de
Jesse Horner <jrhorner@pm.me>
2022-12-12 13:36:42 -0500

    Day 12 complete

---
+++ 2022/go/day12/12_test.go
@@ -0,0 +1,25 @@
+package day12
+
+import (
+	"testing"
+)
+
+***REMOVED***
+
+func Test_p1(t *testing.T) {
+	input := []byte(example)
+	got := Puzzle(&input, false)
+	want := int(31)
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func Test_p2(t *testing.T) {
+	input := []byte(example)
+	got := Puzzle(&input, true)
+	want := int(29)
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
