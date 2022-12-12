f2238353df549aa86969d0751aa0659c4eb98c05
Jesse Horner <jrhorner@pm.me>
2022-12-12 13:36:42 -0500

    Day 12 complete

---
+++ 2022/go/day12/12_test.go
@@ -0,0 +1,30 @@
+package day12
+
+import (
+	"testing"
+)
+
+const example string = `Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi
+`
+
+func Test_p1(t *testing.T) {
+	input := []byte(example)
+	got := Puzzle(&input, false)
+	want := int(31)
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func Test_p2(t *testing.T) {
+	input := []byte(example)
+	got := Puzzle(&input, true)
+	want := int(29)
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
