2e49097a26656908a78faa0c2608f08fad3dd924
Jesse Horner <jrhorner@pm.me>
2022-12-13 15:40:52 -0500

    Corrected template output for part 2

--- templates/go/make_new.sh
+++ templates/go/make_new.sh
@@ -22,7 +22,7 @@ if [[ ! -f ${year}/go/day${day}/${day}.go ]]; then
     sed ${sed_opts} -e "s|package day0|package day${day}|" ${year}/go/day${day}/${day}.go
     sed ${sed_opts} \
         -e "s|^\tlog \"github.com/sirupsen/logrus\"|\t\"${repo}/${year}/go/day${day}\"\n\tlog \"github.com/sirupsen/logrus\"|" \
-        -e "s|^\tdefault:|\tcase ${day}:\n\t\tinput, _ := ioutil.ReadFile(\"${year}/input/${day}\")\n\t\toutput(year, day, 1, fmt.Sprint(day${day}.Puzzle(\&input, false)))\n\t\toutput(year, day, 1, fmt.Sprint(day${day}.Puzzle(\&input, true)))\n\tdefault:|" \
+        -e "s|^\tdefault:|\tcase ${day}:\n\t\tinput, _ := ioutil.ReadFile(\"${year}/input/${day}\")\n\t\toutput(year, day, 1, fmt.Sprint(day${day}.Puzzle(\&input, false)))\n\t\toutput(year, day, 2, fmt.Sprint(day${day}.Puzzle(\&input, true)))\n\tdefault:|" \
         ${year}/go/${year}.go
 fi
 if [[ ! -f ${year}/go/day${day}/${day}_test.go ]]; then
