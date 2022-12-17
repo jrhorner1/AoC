5f1601f566eacab9c8699047215ac189dbc9d1e8
Jesse Horner <jrhorner@pm.me>
2022-12-17 13:43:20 -0500

    Day 16 complete

---
+++ 2022/go/day16/16_test.go
@@ -0,0 +1,35 @@
+package day16
+
+import (
+	"testing"
+)
+
+const example string = `Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II
+`
+
+func Test_p1(t *testing.T) {
+	input := []byte(example)
+	got := Puzzle(&input, false)
+	want := int(1651)
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func Test_p2(t *testing.T) {
+	input := []byte(example)
+	got := Puzzle(&input, true)
+	want := int(1707)
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
