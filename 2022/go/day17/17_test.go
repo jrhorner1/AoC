a20546dfcef7ac92db2a10093bfca35f92c71b29
Jesse Horner <jrhorner@pm.me>
2022-12-19 17:25:56 -0500

    Day 17 part 1 complete

--- 2022/go/day17/17_test.go
+++ 2022/go/day17/17_test.go
@@ -4,13 +4,13 @@ import (
 	"testing"
 )
 
-const example string = `
+const example string = `>>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>
 `
 
 func Test_p1(t *testing.T) {
 	input := []byte(example)
-	got := Puzzle(&input, false)
-	want := int(5 / 7)
+	got := Puzzle(&input, 2022)
+	want := int(3068)
 	if got != want {
 		t.Errorf("got %d, wanted %d", got, want)
 	}
@@ -18,9 +18,13 @@ func Test_p1(t *testing.T) {
 
 func Test_p2(t *testing.T) {
 	input := []byte(example)
-	got := Puzzle(&input, true)
-	want := 42
+	got := Puzzle(&input, 1000000000000)
+	want := int(1514285714288)
 	if got != want {
 		t.Errorf("got %d, wanted %d", got, want)
 	}
 }
+
+/*
+8170 days to calculate by iterating through each step using bitfield
+*/
