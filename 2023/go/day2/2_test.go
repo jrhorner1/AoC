5502eb3bdc4788408cd333ad99c6836e1a3c8dcc
Jesse Horner <jrhorner@pm.me>
2023-12-02 09:53:38 -0500

    Day 2 done

---
+++ 2023/go/day2/2_test.go
@@ -0,0 +1,30 @@
+package day2
+
+import (
+	"testing"
+)
+
+const example string = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+`
+
+func Test_p1(t *testing.T) {
+	input := []byte(example)
+	got := Puzzle(&input, false)
+	want := 8
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func Test_p2(t *testing.T) {
+	input := []byte(example)
+	got := Puzzle(&input, true)
+	want := 2286
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
