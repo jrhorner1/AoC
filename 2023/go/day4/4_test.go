0f6320eb22c75555a6f8b241e29fc69c784efd41
Jesse Horner <jrhorner@pm.me>
2023-12-04 09:36:37 -0500

    day 4 in the bag

---
+++ 2023/go/day4/4_test.go
@@ -0,0 +1,31 @@
+package day4
+
+import (
+	"testing"
+)
+
+const example string = `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+`
+
+func Test_p1(t *testing.T) {
+	input := []byte(example)
+	got := Puzzle(&input, false)
+	want := 13
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func Test_p2(t *testing.T) {
+	input := []byte(example)
+	got := Puzzle(&input, true)
+	want := 30
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
