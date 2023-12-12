a9e7a8c1c7043b49f835a7589fab17447ae0fe61
Jesse Horner <jrhorner@pm.me>
2023-12-12 11:22:31 -0500

    day 11 complete

---
+++ 2023/go/day11/11_test.go
@@ -0,0 +1,49 @@
+package day11
+
+import (
+	"testing"
+
+	log "github.com/sirupsen/logrus"
+)
+
+const example string = `...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....
+`
+
+func Test_p1(t *testing.T) {
+	log.SetLevel(log.DebugLevel)
+	input := []byte(example)
+	got := Puzzle(&input, 2)
+	want := 374
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func Test_p2_1(t *testing.T) {
+	log.SetLevel(log.DebugLevel)
+	input := []byte(example)
+	got := Puzzle(&input, 10)
+	want := 1030
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func Test_p2_2(t *testing.T) {
+	log.SetLevel(log.DebugLevel)
+	input := []byte(example)
+	got := Puzzle(&input, 100)
+	want := 8410
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
