38643aacd178e541bd4fc81dd08bb7cab0a3aeba
Jesse Horner <jrhorner@pm.me>
2023-12-14 11:55:50 -0500

    day 12 part 1 done, part 2 takes too long

---
+++ 2023/go/day12/12_test.go
@@ -0,0 +1,35 @@
+package day12
+
+import (
+	"testing"
+
+	log "github.com/sirupsen/logrus"
+)
+
+const example string = `???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1
+`
+
+func Test_p1(t *testing.T) {
+	log.SetLevel(log.DebugLevel)
+	input := []byte(example)
+	got := Puzzle(&input, false)
+	want := 21
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func Test_p2(t *testing.T) {
+	log.SetLevel(log.DebugLevel)
+	input := []byte(example)
+	got := Puzzle(&input, true)
+	want := 525152
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
